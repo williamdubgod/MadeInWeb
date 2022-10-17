a2a39fef20add8553684749f722376759a617a18
williamdubgod <william_vulcano@hotmail.com>
2022-10-17 16:02:59 -0300

    quebra senha ON

--- Exercicios-MADE/14.py
+++ Exercicios-MADE/14.py
@@ -5,12 +5,8 @@ arraySenha = []
 
 for indexSenha in range(0, senhaLength, 1):
     arraySenha.append(senha[indexSenha])
-    
-while senhaLength != 4:
-    print("senha inválida")
-    senha = input("Digite uma senha com 3 digitos uma letra: ")
 
-caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0','1','2','3','4','5','6','7','8','9']
+caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0','1','2','3','4','5','6','7','8','9', 'ç', '!', '@', '#', '$', '%', '&','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ç']
 combinacoes = len(caracteres)
 
 hack = ''
